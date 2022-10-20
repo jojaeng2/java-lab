893674e8571b7615a480f522680b2f2c4e3efc47
jojaeng2 <ds4ouj@naver.com>
2022-10-20 22:19:40 +0900

    feat : 게임에 사용할 숫자 조합을 생성하는 클래스 생성

---
+++ woowatech/java-baseball-precourse/src/main/java/baseball/util/NumCombinator.java
@@ -0,0 +1,26 @@
+package baseball.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NumCombinator {
+    private final Set<Integer> numbers = new HashSet<>();
+
+    private NumCombinator() {
+
+    }
+
+    public Set<Integer> selectNumbers() {
+        while(isSelectedEnd()) {
+            int number = Randoms.pickNumberInRange(1, 9);
+            numbers.add(number);
+        }
+        return this.numbers;
+    }
+
+    private boolean isSelectedEnd() {
+        return this.numbers.size() == 3;
+    }
+}
