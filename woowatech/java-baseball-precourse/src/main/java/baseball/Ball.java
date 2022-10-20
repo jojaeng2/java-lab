d74b5abea1eb78d0f5d1929a1077702cd7b2097e
jojaeng2 <ds4ouj@naver.com>
2022-10-20 22:25:47 +0900

    feat : 선택된 숫자 조합을 주입받는 생성자 구현

---
+++ woowatech/java-baseball-precourse/src/main/java/baseball/Ball.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Ball {
+    private final Set<Integer> numbers;
+
+    public Ball(Set<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    
+}
